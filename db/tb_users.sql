9577627c438e6e5796417cc4d97663cb53d4fde4
GotohHitori <hitori150221@outlook.com>
2024-06-25 20:02:35 +0800

    项目数据库表结构设计

--- db/tb_users.sql
+++ db/tb_users.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80100 (8.1.0)
  File Encoding         : 65001
 
- Date: 24/06/2024 21:43:36
+ Date: 25/06/2024 20:01:54
 */
 
 SET NAMES utf8mb4;
@@ -33,11 +33,14 @@ CREATE TABLE `tb_users`  (
   `update_by` bigint NULL DEFAULT NULL COMMENT '修改人',
   `update_time` datetime NULL DEFAULT NULL COMMENT '修改时间',
   `create_by` bigint NULL DEFAULT NULL COMMENT '创建人',
+  `group_id` int NULL DEFAULT NULL COMMENT '报名组别',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_users
 -- ----------------------------
+INSERT INTO `tb_users` VALUES (1, '测试', '测试学校', 1, NULL, '测试修改', '123456', '2024-06-25 19:26:07', NULL, '2024-06-25 19:29:28', NULL, NULL);
+INSERT INTO `tb_users` VALUES (2, '测试', '测试学校', 1, NULL, '测试账号2', '123456', '2024-06-25 19:28:58', NULL, NULL, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
